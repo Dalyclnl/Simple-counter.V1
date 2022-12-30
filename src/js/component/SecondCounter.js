5d46e79a86192c99b0f2762dc692175abca0e406
Dalyclnl <dalyarmijoa@yahoo.com>
2022-12-30 18:57:51 +0000

    detalle final

--- src/js/component/SecondCounter.js
+++ src/js/component/SecondCounter.js
@@ -1,38 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import PropTypes  from "prop-types";
 
 
-function Counter (props) {
+function SimpleCounter(props){ 
+	return (
+		<div className="text-center">
+			<div className ="bigCounter">
+				<div className="Clock">
+				   <i className="fa-regular fa-clock"></i>
+				</div>
+				<div className="digit" id="four">{props.digitFour} 1</div>
+				<div className="digit" id="three">{props.digitThree} 2 </div>
+				<div className="digit" id="two"> {props.digitTwo} 3 </div>
+				<div className="digit" id="one">{props.digitOne} 4</div>
+			</div>
+		 </div>);
+ }
 
-    const [time, setTime ] = useState({m:0, s:0, ms:0});
-    //const [interv, setInterv ] =useState ();
+ SimpleCounter.propTypes ={
+	digitFour:PropTypes.number,
+	digitThree:PropTypes.number,
+	digitTwo:PropTypes.number,
+	digitOne:PropTypes.number
+ };
 
-    const start =() => { console.log(start)
-        TimeOn( );
-        setInterval(setInterval(TimeOn,10));
-        };  
 
-     var  timecounterM = time_m  ,timecounterS = time_s ,timecounterMS = time_ms;
 
-        const TimeOn = ( )=>{
-        if (timecounterM === 59){
-            timecounterM(timecounterM +1);
-            return
-            }
-        
-        if(timecounterS === 60){
-            timecounterM++;
-            timecounterS = 0;
-            }
-        }
-        if(timecounterMS === 100){
-            timecounterS++;
-            timecounterMS = 0;
-            }
-            timecounterMS++
-            return setTime ({timecounterM:0, timecounterS:0, timecounterMS:0});
-            
-            console.log(TimeOn)
-    
-    };    
 
-export default Counter;
+export default SimpleCounter;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,28 @@
 import React from "react";
-import Counter from "./SecondCounter";
-import PropTypes from 'prop-types';
-
+import SimpleCounter from "./SecondCounter";
 
 //create your first component
 const Home = () => {
-	    return (
-			<div className="container">
-			   <div className="card body">
-					<span>{props.time_m}</span> 
-					<span>{props.time_s}</span> 
-					<span>{props.time_ms}</span> 
-				</div>	
+	return (
+		<div className="text-center">
+			<div className ="bigCounter">
+			 <SimpleCounter/>
 			</div>
-	);
- };
+		</div>);
+   }
+
+
+ let counter = 0;
+ setInterval(function(){
+	   const four= Math.floor(counter/10000);
+	   const three= Math.floor(counter/1000);
+	   const two= Math.floor(counter/100);
+	   const one= Math.floor(counter/10);
+	   
+	   console.log (four,three,two,one);
+	   counter ++;	},1000);
+
+
+
 
 export default Home;
-
-
-Counter.propTypes = {
-	time_m: PropTypes.number,
-	time_s: PropTypes.number,
-	time_ms: PropTypes.number,
-};
